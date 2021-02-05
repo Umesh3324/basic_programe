7a10b0bd703c6e06ba6c353b384e99eff08aa6dd
Umesh3324 <umesh.prajapati297@gmail.com>
2021-02-05 15:24:26 +0530

    initial version

---
+++ AllData.java
@@ -0,0 +1,30 @@
+package JRays;
+
+public class AllData {
+	//Circle c= new Circle();
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=10;
+		System.out.println(i);
+	//int table[1]= {4};
+		double sum =1+2;
+		System.out.println(sum);
+		double sqrtVal= Math.sqrt(4);
+		System.out.println(sqrtVal);
+	//	a=x*y*z;
+		String name="Umesh kumar";
+		System.out.println(name);
+		double pi=3.14;
+		System.out.println(pi);
+		int a= Math.max(5,2);
+		System.out.println(a);
+		System.out.println(name.length());
+		String n= new String();
+	//0	n.copyValueOf(null)(name);
+		System.out.println(n);
+		
+
+	}
+
+}
---
+++ Fact.java
@@ -0,0 +1,16 @@
+package JRays;
+
+public class Fact {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5,i,fact=1;
+		for(i=1;i<=a;i++)
+		{
+			fact=fact*i;
+		}
+		System.out.println("The factorial of given number 5 is : "+fact);
+		
+	}
+
+}
